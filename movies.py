731c78e884d986e63cee46ae0bdc97c7463c11bb
WMalker808 <walker.maxton@gmail.com>
2024-12-22 12:04:53 +0000

    rearranged order

--- movies.py
+++ movies.py
@@ -7,9 +7,9 @@ MOVIES = [
             {"type": "Genre", "value": "Action/Crime/Drama"},
             {"type": "Box Office", "value": "$1,004.6M"},
             {"type": "Oscar Wins", "value": "2 (Including Supporting Actor)"},
-            {"type": "Director", "value": "Christopher Nolan"},
             {"type": "Famous Quote", "value": "Why so serious?"},
-            {"type": "Lead Actor", "value": "Christian Bale"}
+            {"type": "Lead Actor", "value": "Christian Bale"},
+            {"type": "Director", "value": "Christopher Nolan"}
         ]
     },
     {
@@ -20,9 +20,9 @@ MOVIES = [
             {"type": "Genre", "value": "Thriller/Adventure"},
             {"type": "Box Office", "value": "$470M"},
             {"type": "Oscar Wins", "value": "3 (Including Film Editing)"},
-            {"type": "Director", "value": "Steven Spielberg"},
             {"type": "Famous Quote", "value": "You're gonna need a bigger boat"},
-            {"type": "Lead Actor", "value": "Roy Scheider"}
+            {"type": "Lead Actor", "value": "Roy Scheider"},
+            {"type": "Director", "value": "Steven Spielberg"}
         ]
     },
     {
@@ -33,9 +33,9 @@ MOVIES = [
             {"type": "Genre", "value": "Crime/Drama"},
             {"type": "Box Office", "value": "$245M"},
             {"type": "Oscar Wins", "value": "3 (Including Best Picture)"},
-            {"type": "Director", "value": "Francis Ford Coppola"},
             {"type": "Famous Quote", "value": "I'm gonna make him an offer he can't refuse"},
-            {"type": "Lead Actor", "value": "Marlon Brando"}
+            {"type": "Lead Actor", "value": "Marlon Brando"},
+            {"type": "Director", "value": "Francis Ford Coppola"}
         ]
     },
     {
@@ -46,9 +46,9 @@ MOVIES = [
             {"type": "Genre", "value": "Crime/Drama"},
             {"type": "Box Office", "value": "$213.9M"},
             {"type": "Oscar Wins", "value": "1 (Including Original Screenplay)"},
-            {"type": "Director", "value": "Quentin Tarantino"},
             {"type": "Famous Quote", "value": "Royale with cheese"},
-            {"type": "Lead Actor", "value": "John Travolta"}
+            {"type": "Lead Actor", "value": "John Travolta"},
+            {"type": "Director", "value": "Quentin Tarantino"}
         ]
     },
     {
@@ -59,9 +59,9 @@ MOVIES = [
             {"type": "Genre", "value": "Sci-Fi/Action"},
             {"type": "Box Office", "value": "$463.5M"},
             {"type": "Oscar Wins", "value": "4 (Including Visual Effects)"},
-            {"type": "Director", "value": "The Wachowskis"},
             {"type": "Famous Quote", "value": "I know kung fu"},
-            {"type": "Lead Actor", "value": "Keanu Reeves"}
+            {"type": "Lead Actor", "value": "Keanu Reeves"},
+            {"type": "Director", "value": "The Wachowskis"}
         ]
     },
     {
@@ -72,9 +72,9 @@ MOVIES = [
             {"type": "Genre", "value": "Drama/Romance"},
             {"type": "Box Office", "value": "$678.2M"},
             {"type": "Oscar Wins", "value": "6 (Including Best Picture)"},
-            {"type": "Director", "value": "Robert Zemeckis"},
             {"type": "Famous Quote", "value": "Life is like a box of chocolates"},
-            {"type": "Lead Actor", "value": "Tom Hanks"}
+            {"type": "Lead Actor", "value": "Tom Hanks"},
+            {"type": "Director", "value": "Robert Zemeckis"}
         ]
     },
     {
@@ -85,9 +85,9 @@ MOVIES = [
             {"type": "Genre", "value": "Sci-Fi/Adventure"},
             {"type": "Box Office", "value": "$914M"},
             {"type": "Oscar Wins", "value": "3 (Including Visual Effects)"},
-            {"type": "Director", "value": "Steven Spielberg"},
             {"type": "Famous Quote", "value": "Life finds a way"},
-            {"type": "Lead Actor", "value": "Sam Neill"}
+            {"type": "Lead Actor", "value": "Sam Neill"},
+            {"type": "Director", "value": "Steven Spielberg"}
         ]
     },
     {
@@ -98,9 +98,9 @@ MOVIES = [
             {"type": "Genre", "value": "Romance/Drama"},
             {"type": "Box Office", "value": "$2.2B"},
             {"type": "Oscar Wins", "value": "11 (Including Best Picture)"},
-            {"type": "Director", "value": "James Cameron"},
             {"type": "Famous Quote", "value": "I'm king of the world!"},
-            {"type": "Lead Actor", "value": "Leonardo DiCaprio"}
+            {"type": "Lead Actor", "value": "Leonardo DiCaprio"},
+            {"type": "Director", "value": "James Cameron"}
         ]
     },
     {
@@ -111,9 +111,9 @@ MOVIES = [
             {"type": "Genre", "value": "Drama"},
             {"type": "Box Office", "value": "$58.8M"},
             {"type": "Oscar Wins", "value": "0"},
-            {"type": "Director", "value": "Frank Darabont"},
             {"type": "Famous Quote", "value": "Get busy living, or get busy dying"},
-            {"type": "Lead Actor", "value": "Tim Robbins"}
+            {"type": "Lead Actor", "value": "Tim Robbins"},
+            {"type": "Director", "value": "Frank Darabont"}
         ]
     },
     {
@@ -124,9 +124,9 @@ MOVIES = [
             {"type": "Genre", "value": "Sci-Fi/Action"},
             {"type": "Box Office", "value": "$2.9B"},
             {"type": "Oscar Wins", "value": "3 (Including Art Direction)"},
-            {"type": "Director", "value": "James Cameron"},
             {"type": "Famous Quote", "value": "I see you"},
-            {"type": "Lead Actor", "value": "Sam Worthington"}
+            {"type": "Lead Actor", "value": "Sam Worthington"},
+            {"type": "Director", "value": "James Cameron"}
         ]
     },
     {
@@ -137,9 +137,9 @@ MOVIES = [
             {"type": "Genre", "value": "Sci-Fi/Family"},
             {"type": "Box Office", "value": "$792.9M"},
             {"type": "Oscar Wins", "value": "4 (Including Original Score)"},
-            {"type": "Director", "value": "Steven Spielberg"},
             {"type": "Famous Quote", "value": "E.T. phone home"},
-            {"type": "Lead Actor", "value": "Henry Thomas"}
+            {"type": "Lead Actor", "value": "Henry Thomas"},
+            {"type": "Director", "value": "Steven Spielberg"}
         ]
     },
     {
@@ -150,9 +150,9 @@ MOVIES = [
             {"type": "Genre", "value": "Crime/Drama"},
             {"type": "Box Office", "value": "$47.1M"},
             {"type": "Oscar Wins", "value": "1 (Including Supporting Actor)"},
-            {"type": "Director", "value": "Martin Scorsese"},
             {"type": "Famous Quote", "value": "As far back as I can remember, I always wanted to be a gangster"},
-            {"type": "Lead Actor", "value": "Ray Liotta"}
+            {"type": "Lead Actor", "value": "Ray Liotta"},
+            {"type": "Director", "value": "Martin Scorsese"}
         ]
     },
     {
@@ -163,9 +163,9 @@ MOVIES = [
             {"type": "Genre", "value": "Sci-Fi/Adventure"},
             {"type": "Box Office", "value": "$775M"},
             {"type": "Oscar Wins", "value": "6 (Including Art Direction)"},
-            {"type": "Director", "value": "George Lucas"},
             {"type": "Famous Quote", "value": "May the Force be with you"},
-            {"type": "Lead Actor", "value": "Mark Hamill"}
+            {"type": "Lead Actor", "value": "Mark Hamill"},
+            {"type": "Director", "value": "George Lucas"}
         ]
     },
     {
@@ -176,9 +176,9 @@ MOVIES = [
             {"type": "Genre", "value": "Biography/Drama"},
             {"type": "Box Office", "value": "$322.2M"},
             {"type": "Oscar Wins", "value": "7 (Including Best Picture)"},
-            {"type": "Director", "value": "Steven Spielberg"},
             {"type": "Famous Quote", "value": "Whoever saves one life saves the world entire"},
-            {"type": "Lead Actor", "value": "Liam Neeson"}
+            {"type": "Lead Actor", "value": "Liam Neeson"},
+            {"type": "Director", "value": "Steven Spielberg"}
         ]
     },
     {
@@ -189,9 +189,9 @@ MOVIES = [
             {"type": "Genre", "value": "Drama/Thriller"},
             {"type": "Box Office", "value": "$101.2M"},
             {"type": "Oscar Wins", "value": "0"},
-            {"type": "Director", "value": "David Fincher"},
             {"type": "Famous Quote", "value": "The first rule of Fight Club is: you do not talk about Fight Club"},
-            {"type": "Lead Actor", "value": "Brad Pitt"}
+            {"type": "Lead Actor", "value": "Brad Pitt"},
+            {"type": "Director", "value": "David Fincher"}
         ]
     },
     {
@@ -202,9 +202,9 @@ MOVIES = [
             {"type": "Genre", "value": "Crime/Thriller"},
             {"type": "Box Office", "value": "$272.7M"},
             {"type": "Oscar Wins", "value": "5 (Including Best Picture)"},
-            {"type": "Director", "value": "Jonathan Demme"},
             {"type": "Famous Quote", "value": "A census taker once tried to test me. I ate his liver with some fava beans"},
-            {"type": "Lead Actor", "value": "Jodie Foster"}
+            {"type": "Lead Actor", "value": "Jodie Foster"},
+            {"type": "Director", "value": "Jonathan Demme"}
         ]
     },
     {
@@ -215,9 +215,9 @@ MOVIES = [
             {"type": "Genre", "value": "Sci-Fi/Adventure"},
             {"type": "Box Office", "value": "$388.8M"},
             {"type": "Oscar Wins", "value": "1 (Including Sound Effects)"},
-            {"type": "Director", "value": "Robert Zemeckis"},
             {"type": "Famous Quote", "value": "Where we're going, we don't need roads"},
-            {"type": "Lead Actor", "value": "Michael J. Fox"}
+            {"type": "Lead Actor", "value": "Michael J. Fox"},
+            {"type": "Director", "value": "Robert Zemeckis"}
         ]
     },
     {
@@ -228,9 +228,9 @@ MOVIES = [
             {"type": "Genre", "value": "Animation/Drama"},
             {"type": "Box Office", "value": "$968.5M"},
             {"type": "Oscar Wins", "value": "2 (Including Original Score)"},
-            {"type": "Director", "value": "Roger Allers"},
             {"type": "Famous Quote", "value": "Hakuna matata"},
-            {"type": "Lead Actor", "value": "Matthew Broderick"}
+            {"type": "Lead Actor", "value": "Matthew Broderick"},
+            {"type": "Director", "value": "Roger Allers"}
         ]
     },
     {
@@ -241,9 +241,9 @@ MOVIES = [
             {"type": "Genre", "value": "Action/Drama"},
             {"type": "Box Office", "value": "$460.6M"},
             {"type": "Oscar Wins", "value": "5 (Including Best Picture)"},
-            {"type": "Director", "value": "Ridley Scott"},
             {"type": "Famous Quote", "value": "Are you not entertained?"},
-            {"type": "Lead Actor", "value": "Russell Crowe"}
+            {"type": "Lead Actor", "value": "Russell Crowe"},
+            {"type": "Director", "value": "Ridley Scott"}
         ]
     },
     {
@@ -254,9 +254,9 @@ MOVIES = [
             {"type": "Genre", "value": "Fantasy/Adventure"},
             {"type": "Box Office", "value": "$897.7M"},
             {"type": "Oscar Wins", "value": "4 (Including Cinematography)"},
-            {"type": "Director", "value": "Peter Jackson"},
             {"type": "Famous Quote", "value": "You shall not pass!"},
-            {"type": "Lead Actor", "value": "Elijah Wood"}
+            {"type": "Lead Actor", "value": "Elijah Wood"},
+            {"type": "Director", "value": "Peter Jackson"}
         ]
     }
 ]
