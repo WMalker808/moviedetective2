c05fc60258f10056f220b259fd7b3fc27620edb7
WMalker808 <walker.maxton@gmail.com>
2024-12-22 11:58:53 +0000

    added more info and rearranged order of clues

--- movies.py
+++ movies.py
@@ -3,10 +3,10 @@ MOVIES = [
         "title": "The Dark Knight",
         "clues": [
             {"type": "Year", "value": "2008"},
+            {"type": "Supporting Actor", "value": "Michael Caine"},
             {"type": "Genre", "value": "Action/Crime/Drama"},
-            {"type": "Runtime", "value": "152 minutes"},
             {"type": "Box Office", "value": "$1,004.6M"},
-            {"type": "Oscar Wins", "value": "2"},
+            {"type": "Oscar Wins", "value": "2 (Including Supporting Actor)"},
             {"type": "Director", "value": "Christopher Nolan"},
             {"type": "Famous Quote", "value": "Why so serious?"},
             {"type": "Lead Actor", "value": "Christian Bale"}
@@ -16,10 +16,10 @@ MOVIES = [
         "title": "Jaws",
         "clues": [
             {"type": "Year", "value": "1975"},
+            {"type": "Supporting Actor", "value": "Richard Dreyfuss"},
             {"type": "Genre", "value": "Thriller/Adventure"},
-            {"type": "Runtime", "value": "124 minutes"},
             {"type": "Box Office", "value": "$470M"},
-            {"type": "Oscar Wins", "value": "3"},
+            {"type": "Oscar Wins", "value": "3 (Including Film Editing)"},
             {"type": "Director", "value": "Steven Spielberg"},
             {"type": "Famous Quote", "value": "You're gonna need a bigger boat"},
             {"type": "Lead Actor", "value": "Roy Scheider"}
@@ -29,10 +29,10 @@ MOVIES = [
         "title": "The Godfather",
         "clues": [
             {"type": "Year", "value": "1972"},
+            {"type": "Supporting Actor", "value": "James Caan"},
             {"type": "Genre", "value": "Crime/Drama"},
-            {"type": "Runtime", "value": "175 minutes"},
             {"type": "Box Office", "value": "$245M"},
-            {"type": "Oscar Wins", "value": "3"},
+            {"type": "Oscar Wins", "value": "3 (Including Best Picture)"},
             {"type": "Director", "value": "Francis Ford Coppola"},
             {"type": "Famous Quote", "value": "I'm gonna make him an offer he can't refuse"},
             {"type": "Lead Actor", "value": "Marlon Brando"}
@@ -42,10 +42,10 @@ MOVIES = [
         "title": "Pulp Fiction",
         "clues": [
             {"type": "Year", "value": "1994"},
+            {"type": "Supporting Actor", "value": "Bruce Willis"},
             {"type": "Genre", "value": "Crime/Drama"},
-            {"type": "Runtime", "value": "154 minutes"},
             {"type": "Box Office", "value": "$213.9M"},
-            {"type": "Oscar Wins", "value": "1"},
+            {"type": "Oscar Wins", "value": "1 (Including Original Screenplay)"},
             {"type": "Director", "value": "Quentin Tarantino"},
             {"type": "Famous Quote", "value": "Royale with cheese"},
             {"type": "Lead Actor", "value": "John Travolta"}
@@ -55,10 +55,10 @@ MOVIES = [
         "title": "The Matrix",
         "clues": [
             {"type": "Year", "value": "1999"},
+            {"type": "Supporting Actor", "value": "Joe Pantoliano"},
             {"type": "Genre", "value": "Sci-Fi/Action"},
-            {"type": "Runtime", "value": "136 minutes"},
             {"type": "Box Office", "value": "$463.5M"},
-            {"type": "Oscar Wins", "value": "4"},
+            {"type": "Oscar Wins", "value": "4 (Including Visual Effects)"},
             {"type": "Director", "value": "The Wachowskis"},
             {"type": "Famous Quote", "value": "I know kung fu"},
             {"type": "Lead Actor", "value": "Keanu Reeves"}
@@ -68,10 +68,10 @@ MOVIES = [
         "title": "Forrest Gump",
         "clues": [
             {"type": "Year", "value": "1994"},
+            {"type": "Supporting Actor", "value": "Gary Sinise"},
             {"type": "Genre", "value": "Drama/Romance"},
-            {"type": "Runtime", "value": "142 minutes"},
             {"type": "Box Office", "value": "$678.2M"},
-            {"type": "Oscar Wins", "value": "6"},
+            {"type": "Oscar Wins", "value": "6 (Including Best Picture)"},
             {"type": "Director", "value": "Robert Zemeckis"},
             {"type": "Famous Quote", "value": "Life is like a box of chocolates"},
             {"type": "Lead Actor", "value": "Tom Hanks"}
@@ -81,10 +81,10 @@ MOVIES = [
         "title": "Jurassic Park",
         "clues": [
             {"type": "Year", "value": "1993"},
+            {"type": "Supporting Actor", "value": "Wayne Knight"},
             {"type": "Genre", "value": "Sci-Fi/Adventure"},
-            {"type": "Runtime", "value": "127 minutes"},
             {"type": "Box Office", "value": "$914M"},
-            {"type": "Oscar Wins", "value": "3"},
+            {"type": "Oscar Wins", "value": "3 (Including Visual Effects)"},
             {"type": "Director", "value": "Steven Spielberg"},
             {"type": "Famous Quote", "value": "Life finds a way"},
             {"type": "Lead Actor", "value": "Sam Neill"}
@@ -94,10 +94,10 @@ MOVIES = [
         "title": "Titanic",
         "clues": [
             {"type": "Year", "value": "1997"},
+            {"type": "Supporting Actor", "value": "Billy Zane"},
             {"type": "Genre", "value": "Romance/Drama"},
-            {"type": "Runtime", "value": "195 minutes"},
             {"type": "Box Office", "value": "$2.2B"},
-            {"type": "Oscar Wins", "value": "11"},
+            {"type": "Oscar Wins", "value": "11 (Including Best Picture)"},
             {"type": "Director", "value": "James Cameron"},
             {"type": "Famous Quote", "value": "I'm king of the world!"},
             {"type": "Lead Actor", "value": "Leonardo DiCaprio"}
@@ -107,8 +107,8 @@ MOVIES = [
         "title": "The Shawshank Redemption",
         "clues": [
             {"type": "Year", "value": "1994"},
+            {"type": "Supporting Actor", "value": "William Sadler"},
             {"type": "Genre", "value": "Drama"},
-            {"type": "Runtime", "value": "142 minutes"},
             {"type": "Box Office", "value": "$58.8M"},
             {"type": "Oscar Wins", "value": "0"},
             {"type": "Director", "value": "Frank Darabont"},
@@ -120,10 +120,10 @@ MOVIES = [
         "title": "Avatar",
         "clues": [
             {"type": "Year", "value": "2009"},
+            {"type": "Supporting Actor", "value": "Joel David Moore"},
             {"type": "Genre", "value": "Sci-Fi/Action"},
-            {"type": "Runtime", "value": "162 minutes"},
             {"type": "Box Office", "value": "$2.9B"},
-            {"type": "Oscar Wins", "value": "3"},
+            {"type": "Oscar Wins", "value": "3 (Including Art Direction)"},
             {"type": "Director", "value": "James Cameron"},
             {"type": "Famous Quote", "value": "I see you"},
             {"type": "Lead Actor", "value": "Sam Worthington"}
@@ -133,10 +133,10 @@ MOVIES = [
         "title": "E.T. the Extra-Terrestrial",
         "clues": [
             {"type": "Year", "value": "1982"},
+            {"type": "Supporting Actor", "value": "Peter Coyote"},
             {"type": "Genre", "value": "Sci-Fi/Family"},
-            {"type": "Runtime", "value": "115 minutes"},
             {"type": "Box Office", "value": "$792.9M"},
-            {"type": "Oscar Wins", "value": "4"},
+            {"type": "Oscar Wins", "value": "4 (Including Original Score)"},
             {"type": "Director", "value": "Steven Spielberg"},
             {"type": "Famous Quote", "value": "E.T. phone home"},
             {"type": "Lead Actor", "value": "Henry Thomas"}
@@ -146,10 +146,10 @@ MOVIES = [
         "title": "Goodfellas",
         "clues": [
             {"type": "Year", "value": "1990"},
+            {"type": "Supporting Actor", "value": "Paul Sorvino"},
             {"type": "Genre", "value": "Crime/Drama"},
-            {"type": "Runtime", "value": "146 minutes"},
             {"type": "Box Office", "value": "$47.1M"},
-            {"type": "Oscar Wins", "value": "1"},
+            {"type": "Oscar Wins", "value": "1 (Including Supporting Actor)"},
             {"type": "Director", "value": "Martin Scorsese"},
             {"type": "Famous Quote", "value": "As far back as I can remember, I always wanted to be a gangster"},
             {"type": "Lead Actor", "value": "Ray Liotta"}
@@ -159,10 +159,10 @@ MOVIES = [
         "title": "Star Wars: Episode IV - A New Hope",
         "clues": [
             {"type": "Year", "value": "1977"},
+            {"type": "Supporting Actor", "value": "Peter Cushing"},
             {"type": "Genre", "value": "Sci-Fi/Adventure"},
-            {"type": "Runtime", "value": "121 minutes"},
             {"type": "Box Office", "value": "$775M"},
-            {"type": "Oscar Wins", "value": "6"},
+            {"type": "Oscar Wins", "value": "6 (Including Art Direction)"},
             {"type": "Director", "value": "George Lucas"},
             {"type": "Famous Quote", "value": "May the Force be with you"},
             {"type": "Lead Actor", "value": "Mark Hamill"}
@@ -172,10 +172,10 @@ MOVIES = [
         "title": "Schindler's List",
         "clues": [
             {"type": "Year", "value": "1993"},
+            {"type": "Supporting Actor", "value": "Embeth Davidtz"},
             {"type": "Genre", "value": "Biography/Drama"},
-            {"type": "Runtime", "value": "195 minutes"},
             {"type": "Box Office", "value": "$322.2M"},
-            {"type": "Oscar Wins", "value": "7"},
+            {"type": "Oscar Wins", "value": "7 (Including Best Picture)"},
             {"type": "Director", "value": "Steven Spielberg"},
             {"type": "Famous Quote", "value": "Whoever saves one life saves the world entire"},
             {"type": "Lead Actor", "value": "Liam Neeson"}
@@ -185,8 +185,8 @@ MOVIES = [
         "title": "Fight Club",
         "clues": [
             {"type": "Year", "value": "1999"},
+            {"type": "Supporting Actor", "value": "Jared Leto"},
             {"type": "Genre", "value": "Drama/Thriller"},
-            {"type": "Runtime", "value": "139 minutes"},
             {"type": "Box Office", "value": "$101.2M"},
             {"type": "Oscar Wins", "value": "0"},
             {"type": "Director", "value": "David Fincher"},
@@ -198,10 +198,10 @@ MOVIES = [
         "title": "The Silence of the Lambs",
         "clues": [
             {"type": "Year", "value": "1991"},
+            {"type": "Supporting Actor", "value": "Scott Glenn"},
             {"type": "Genre", "value": "Crime/Thriller"},
-            {"type": "Runtime", "value": "118 minutes"},
             {"type": "Box Office", "value": "$272.7M"},
-            {"type": "Oscar Wins", "value": "5"},
+            {"type": "Oscar Wins", "value": "5 (Including Best Picture)"},
             {"type": "Director", "value": "Jonathan Demme"},
             {"type": "Famous Quote", "value": "A census taker once tried to test me. I ate his liver with some fava beans"},
             {"type": "Lead Actor", "value": "Jodie Foster"}
@@ -211,10 +211,10 @@ MOVIES = [
         "title": "Back to the Future",
         "clues": [
             {"type": "Year", "value": "1985"},
+            {"type": "Supporting Actor", "value": "Lea Thompson"},
             {"type": "Genre", "value": "Sci-Fi/Adventure"},
-            {"type": "Runtime", "value": "116 minutes"},
             {"type": "Box Office", "value": "$388.8M"},
-            {"type": "Oscar Wins", "value": "1"},
+            {"type": "Oscar Wins", "value": "1 (Including Sound Effects)"},
             {"type": "Director", "value": "Robert Zemeckis"},
             {"type": "Famous Quote", "value": "Where we're going, we don't need roads"},
             {"type": "Lead Actor", "value": "Michael J. Fox"}
@@ -224,10 +224,10 @@ MOVIES = [
         "title": "The Lion King",
         "clues": [
             {"type": "Year", "value": "1994"},
+            {"type": "Supporting Actor", "value": "Jeremy Irons"},
             {"type": "Genre", "value": "Animation/Drama"},
-            {"type": "Runtime", "value": "88 minutes"},
             {"type": "Box Office", "value": "$968.5M"},
-            {"type": "Oscar Wins", "value": "2"},
+            {"type": "Oscar Wins", "value": "2 (Including Original Score)"},
             {"type": "Director", "value": "Roger Allers"},
             {"type": "Famous Quote", "value": "Hakuna matata"},
             {"type": "Lead Actor", "value": "Matthew Broderick"}
@@ -237,10 +237,10 @@ MOVIES = [
         "title": "Gladiator",
         "clues": [
             {"type": "Year", "value": "2000"},
+            {"type": "Supporting Actor", "value": "Djimon Hounsou"},
             {"type": "Genre", "value": "Action/Drama"},
-            {"type": "Runtime", "value": "155 minutes"},
             {"type": "Box Office", "value": "$460.6M"},
-            {"type": "Oscar Wins", "value": "5"},
+            {"type": "Oscar Wins", "value": "5 (Including Best Picture)"},
             {"type": "Director", "value": "Ridley Scott"},
             {"type": "Famous Quote", "value": "Are you not entertained?"},
             {"type": "Lead Actor", "value": "Russell Crowe"}
@@ -250,10 +250,10 @@ MOVIES = [
         "title": "The Lord of the Rings: The Fellowship of the Ring",
         "clues": [
             {"type": "Year", "value": "2001"},
+            {"type": "Supporting Actor", "value": "Sean Bean"},
             {"type": "Genre", "value": "Fantasy/Adventure"},
-            {"type": "Runtime", "value": "178 minutes"},
             {"type": "Box Office", "value": "$897.7M"},
-            {"type": "Oscar Wins", "value": "4"},
+            {"type": "Oscar Wins", "value": "4 (Including Cinematography)"},
             {"type": "Director", "value": "Peter Jackson"},
             {"type": "Famous Quote", "value": "You shall not pass!"},
             {"type": "Lead Actor", "value": "Elijah Wood"}
